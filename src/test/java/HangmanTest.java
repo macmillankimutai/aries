b3a997149e58e71aa1220e7899b0103a180e3681
macmillan kimutai <mcmyllano@gmail.com>
2018-05-09 16:17:20 +0300

    Wrote the the test file.

---
+++ src/test/java/HangmanTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+   public class HangmanTest {
+     @test
+     public boolean isHangmanTest( int year) {
+       return year % 4 == 0;
+     }
+   }
