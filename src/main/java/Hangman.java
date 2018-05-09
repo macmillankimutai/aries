bc7e494070acac1e7598df4f63550bdd83e84e2d
macmillan kimutai <mcmyllano@gmail.com>
2018-05-09 16:36:16 +0300

    This is to commit that i have imported the array list, the console to the hangman.

---
+++ src/main/java/Hangman.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Hangman {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+  }
+}
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -2,7 +2,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
    public class HangmanTest {
-     @test
+     @Test
      public boolean isHangmanTest( int year) {
        return year % 4 == 0;
      }
