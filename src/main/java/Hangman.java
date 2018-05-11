4b7d24d407468f50ad810d87c30205a1a948439e
macmillan kimutai <mcmyllano@gmail.com>
2018-05-11 03:47:40 +0300

    This is to commit that i have added some changes to the Hangman.java that i have imported the scanner and the console.

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Hangman {
-  public static void main(String[] args) {
-    Console myConsole = System.console();
-  }
+ public static void main(String[] args) {
+   System.out.println("What is your choice?");
+ }
 }
