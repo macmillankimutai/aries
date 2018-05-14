40e1dbb9009a16be8aa36f2790f916483f2923d8
macmillan kimutai <mcmyllano@gmail.com>
2018-05-14 09:53:06 +0300

    This is to commit all the files that i have changed.

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,69 +1,43 @@
-import java.io.Console;
-import java.util.Random;
 import java.util.Scanner;
-
-public class Hangman {
-  private static String[] words = {"youth","boolean","asterisk"};
-  private static String word = words[(int) (Math.random() * words.length)];
-  private static String asterisk = new String(new char[word.length()]).replace("\0","*");
-  private static int count = 0;
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    while (Count < 7 && asterisk.contains("*")) {
-     System.out.println("Guess any letter");
-     System.out.println(asterisk);
-     String guess = sc.next();
-     hang(guess);
+class Hangman
+{
+  public static void main(String[] args)
+  {
+    Scanner guess = new Scanner(System.in);
+    System.out.println("Welcome to Hangman");
+    System.out.println("Difficulty(1-3): ");
+    int diff = guess.nextInt();
+    if(diff==1)
+    {
+      Play obj = new Play();
+        obj.Start(diff);
+    }else{
+      if(diff==2)
+      {
+        Play obj = new Play();
+        obj.Start(diff);
+      }else{
+        Play obj = new Play();
+        obj.Start(3);
+      }
     }
-    sc.close();
+    switch (diff)
+    {
+      case 1:
+        Play obj = new Play();
+        obj.Start(diff);
+        break;
+      case 2:
+        Play obj = new Play();
+        obj.Start(diff);
+        break;
+      case 3:
+        Play obj = new Play();
+        obj.Start(diff);
+        break;
+      default:
+        Play obj = new Play();
+        obj.Start(diff);
+    }
   }
-  public static void hang(String guess) {
-		String newasterisk = "";
-		for (int i = 0; i < word.length(); i++) {
-			if (word.charAt(i) == guess.charAt(0)) {
-				newasterisk += guess.charAt(0);
-			} else if (asterisk.charAt(i) != '*') {
-				newasterisk += word.charAt(i);
-			} else {
-				newasterisk += "*";
-			}
-		}
-
-		if (asterisk.equals(newasterisk)) {
-			count++;
-			hangmanImage();
-		} else {
-			asterisk = newasterisk;
-		}
-		if (asterisk.equals(word)) {
-			System.out.println("Correct! You win! The word was " + word);
-		}
-	}
-
-	public static void hangmanImage() {
-		if (count == 1) {
-			System.out.println("Wrong guess, try again");
-		}
-		if (count == 2) {
-			System.out.println("Wrong guess, try again")
-		}
-		if (count == 3) {
-			System.out.println("Wrong guess, try again");;
-		}
-		if (count == 4) {
-			System.out.println("Wrong guess, try again");
-		}
-		if (count == 5) {
-			System.out.println("Wrong guess, try again");
-		}
-		if (count == 6) {
-			System.out.println("Wrong guess, try again");
-		}
-		if (count == 7) {
-			System.out.println("GAME OVER!");
-			System.out.println("GAME OVER! The word was " + word);
-		}
-	}
 }
