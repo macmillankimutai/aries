8c7be453a6a4c15351ce2c0e5e36cce416cc8d83
macmillan kimutai <mcmyllano@gmail.com>
2018-05-16 09:26:10 +0300

    This is to commit that i hava made some changes to the files.

--- src/test/java/HangmanTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-
-   public class HangmanTest {
-     @Test
-     public boolean isHangmanTest( int year) {
-       return year % 4 == 0;
-     }
-   }
